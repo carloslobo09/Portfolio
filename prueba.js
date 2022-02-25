96fe18e5637f1bc9ed1f6a0cbac7fc92bd01bc00
carloslobo09 <80993527+carloslobo09@users.noreply.github.com>
2022-02-25 18:14:58 -0300

    second commit

---
+++ prueba.js
@@ -0,0 +1,9 @@
+// const frutas = ['manzana','frutilla','sandia']
+// let cont = 0
+// frutas.forEach(fruta => {
+//     if (fruta.length > 6){  
+//         document.write(fruta);
+//         cont++
+//         console.log(cont)
+//     }
+// });
